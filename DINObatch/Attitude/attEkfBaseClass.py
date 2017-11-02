2a3d108cec8a0dc3751a90acad165ef8c6fcbab0
atharris <anha5550@colorado.edu>
2017-11-02 09:50:14 -0600

    More filter class changes
    lots of weird small mistakes on my part.

--- DINObatch/Attitude/attEkfBaseClass.py
+++ DINObatch/Attitude/attEkfBaseClass.py
@@ -1,10 +1,12 @@
 import attEkfAlgs as ekf
 import numpy as np
+from numpy import linalg as la
 import scipy as sci
 import attKalmanAlgs as aka
+import RigidBodyKinematics as rbk
 
 class attitudeEKF:
-    def __init__(self, initState, initCovar, procNoise, measNoise, statePartials, directTransmission, measPartials, measTransmission, dt):
+    def __init__(self, initState, initCovar, procNoise, measNoise, measPartials, measTransmission, dt):
         #! Function spec:
         #! Inputs
         #! initState - 6x1 - initial state vector; 1st 3 are MRP components, second 3 are body/inertial angular rates
@@ -22,28 +24,30 @@ class attitudeEKF:
         self.Q = procNoise
         self.R = measNoise
 
-        self.F = statePartials
-        self.G = directTransmission
         self.H = measPartials
         self.J = measTransmission
 
         self.dt = dt
 
         self.newMeasurements = False
+        self.newGyro = False
+        self.newSt = False
 
     def newGyroMeas(self, gyroMeas):
         self.rateObs = gyroMeas
-        self.newMeasurements = True
+        self.newGyro = True
 
     def newStMeas(self, stMeas):
-        self.self.stObs = stMeas
-        self.newMeasurements = True
+        self.stObs = stMeas
+        self.newSt = True
 
     def UpdateState(self):
+        self.newMeasurements = self.newSt or self.newGyro
         if self.newMeasurements:
-            self.filterCorrect()
+            self.filterUpdate(self.newSt, self.newGyro)
             self.newMeasurements = False
-            self.currentMeas = np.zeros([6,])
+            self.newSt = False
+            self.newGyro = False
         else:
             self.filterProp()
 
@@ -52,21 +56,45 @@ class attitudeEKF:
         self.estState = stm * self.estState
         self.estCovar = np.transpose(stm).dot(self.estCovar).dot(stm)
 
-    def filterUpdate(self):
+    def filterUpdate(self, newSt, newGyro):
         # KF PREDICTION
-
         omega_ref = self.rateObs - self.estState[3:6]
         (A_LS, G_LS) = aka.linearizeSystem(self.estState[0:3], omega_ref)
+
         # Propagate KF State Estimate:
         params_KF = (self.rateObs, A_LS, G_LS, self.Q)
-        predState = aka.RK4(self.estState, ekf.propagate_dynKF, params_KF, dt)
+        predState = aka.RK4(self.estState, ekf.propagate_dynKF, params_KF, self.dt)
         aka.check_ShadowTransform(predState)
 
         # KF CORRECTION
-        (self.estState, e_y) = aka.computeKalmanCorrection(predState, Y_meas_vec[:, k], H, R)
-        E_y_vec[k] = e_y
-        E_y_ergo_vec[k] = float(1.0/(k+1)) * np.sum(E_y_vec[:k])
+        if newSt and newGyro:
+            tmpMeas = self.stMeas
+            tmpH = self.H
+            tmpR = self.R
+            (self.estState, self.estCovar) = aka.computeKalmanCorrection(predState, tmpMeas, tmpH, tmpR)
+        elif newGyro:
+            tmpMeas = self.stMeas
+            self.estState[0:3] = predState[0:3]
+        else:
+            print "Error: This shouldn't have happened."
 
-        check_ShadowTransform(self.estState)
+        (self.estState, self.estCovar) = aka.computeKalmanCorrection(predState, tmpMeas, tmpH, tmpR)
+        rbk.MRPShadow(self.estState, 1.0)
 
+if __name__ == "__main__":
+    (k_f, dt, k_meas) = aka.define_numericData()
+    print 't_sim = ', k_f*dt
 
+    (cov_ARW, cov_RRW) = aka.define_noiseIntensities()
+    print 'cov_ARW = ', cov_ARW
+    print 'cov_RRW = ', cov_RRW
+
+    (sigma0_true, omega_true, bias_true) = aka.initialize_groundTruth()
+    X0_ground = np.append(sigma0_true, bias_true)
+    print 'sigma0_true = ', sigma0_true
+    print 'omega_true = ', omega_true
+    print 'bias_true = ', bias_true
+    W = ekf.define_PSD_Q()
+    X0_est = ekf.define_initialGuess_KF()
+                    #self, initState, initCovar, procNoise, measNoise, statePartials, directTransmission, measPartials, measTransmission, dt
+    ekfClass = attitudeEKF(X0_est, np.identity(6), cov_ARW, cov_RRW,
