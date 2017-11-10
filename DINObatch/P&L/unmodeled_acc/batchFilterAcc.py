Delta Post fit check, upgrades for infinite init covariance mat, plot modifications, prefit fixes, states plotted etc

DINObatch/P&L/unmodeled_acc/batchFilterAcc.py
@@ -126,7 +126,7 @@ def run_batch( input ) :
   # if there exists a priori information. if not, use default
   if np.sum(np.sum( P_bar )) == 0 :
     info_matrix = np.zeros( (n_state,n_state) )
-    normal_matrix = np.zeros( (n_state,) )
+    normal_matrix = np.zeros( (n_state,1) )
  
   else :
     info_matrix = aInv( P_bar )
@@ -188,9 +188,11 @@ def run_batch( input ) :
       H    = np.dot( H_tilde[0+2*ii:2+2*ii,:], phi_t_t0 )
       # add the new H^T H result to the information matrix
       info_matrix   += np.dot( H.T, np.dot( W, H ) )
-      P_array[ii,:,:]   = aInv( info_matrix )
+      # P_array[ii,:,:]   = aInv( info_matrix )
       # add the H^T Y result to the observation information matrix
-      normal_matrix += np.dot( H.T, np.dot( W, np.expand_dims(y[ii,:],axis=1) ) )
+      yii = np.zeros([len( y[ii,:]),1])
+      yii[:,0] = y[ii,:]
+      normal_matrix += np.dot( H.T, np.dot( W, yii))
 
   ##################################################################################
   #
@@ -212,12 +214,14 @@ def run_batch( input ) :
 
   # initiate an array for the state deviation vectors
   x_hat_array = np.zeros( (n_samples, n_state) )
+  x_bar_array = np.zeros( (n_samples, n_state) )
 
   for ii in xrange( ref_state.shape[0] ) :
     # pull the STM that is able to transform from the current time to t0
     phi_t_t0  = np.reshape( ref_state[ii,n_state:], (n_state,n_state) )
     # linearly transform deviation and at it to the ref_state and save
     x_hat_array[ii,:] = np.dot( phi_t_t0, x_hat ).T
+    x_bar_array[ii,:] = np.dot( phi_t_t0, x_bar ).T
     P_array[ii,:,:] = np.dot(np.dot( phi_t_t0, P ),  phi_t_t0.T)
     # add the deviation to the reference state 
     est_state[ii,:]   = ref_state[ii,0:n_state] + x_hat_array[ii,:]
@@ -232,12 +236,16 @@ def run_batch( input ) :
 
   # compute the postfits using the updated observables and the measured values
   postfits = np.zeros([np.shape(x_hat_array)[0], np.shape(y)[1]])
+  postfitsDelta = np.zeros([np.shape(x_hat_array)[0], np.shape(y)[1]])
   for ii in range(np.shape(x_hat_array)[0]):
-    postfits[ii,:] = y[ii,:] - np.dot(H_tilde[0+2*ii:2+2*ii,:], x_hat_array[ii,:])
+    postfitsDelta[ii,:] = extras['oldPost'][ii,:] - y[ii,:] + np.dot(H_tilde[0+2*ii:2+2*ii,:], x_hat_array[ii,0:np.shape(H_tilde)[1]])
+    postfits[ii,:] = y[ii,:] - np.dot(H_tilde[0+2*ii:2+2*ii,:], x_hat_array[ii,0:np.shape(H_tilde)[1]])
+
+
 
   prefits = np.zeros([np.shape(x_hat_array)[0], np.shape(y)[1]])
   for ii in range(1,np.shape(x_hat_array)[0]):
-    prefits[ii,:] = y[ii,:] - np.dot(H_tilde[0+2*(ii):2+2*(ii),:], x_hat_array[ii-1,:])
+    prefits[ii,:] = y[ii,:] - np.dot(H_tilde[0+2*(ii):2+2*(ii),:], x_bar_array[ii-1,:])
 
   # store various arrays in a data dictionary
   extra_data                      = {}
@@ -246,6 +254,7 @@ def run_batch( input ) :
   extra_data['x_hat_array']       = x_hat_array
   extra_data['prefit residuals']  = prefits
   extra_data['postfit residuals'] = postfits
+  extra_data['postfit changes'] = postfitsDelta
   # is this legacy code?
   extras['x_hat_0']               += x_hat
   extra_data['x_hat_0']           = extras['x_hat_0']

DINObatch/P&L/unmodeled_acc/init_acc.py
@@ -228,7 +228,7 @@ def main():
     extras['line_direction'] = 1.
 
     # Are we using the real dynamics for the ref or the trueData
-    extras['realData']= 'OFF'
+    extras['realData']= 'ON'
 
     ##################################################################################
 
@@ -280,15 +280,15 @@ def main():
     
     # a priori uncertainty for the ref_states
     P_bar = np.zeros((IC.shape[0], IC.shape[0]))
-    P_bar[0, 0] = 10000**2
-    P_bar[1, 1] = 10000**2
-    P_bar[2, 2] = 10000**2
-    P_bar[3, 3] = .1**2
-    P_bar[4, 4] = .1**2
-    P_bar[5, 5] = .1**2
-    P_bar[6, 6] = (10**(-8))**2
-    P_bar[7, 7] = (10**(-8))**2
-    P_bar[8, 8] = (10**(-8))**2
+    # P_bar[0, 0] = 10000**2
+    # P_bar[1, 1] = 10000**2
+    # P_bar[2, 2] = 10000**2
+    # P_bar[3, 3] = .1**2
+    # P_bar[4, 4] = .1**2
+    # P_bar[5, 5] = .1**2
+    # P_bar[6, 6] = (10**(-8))**2
+    # P_bar[7, 7] = (10**(-8))**2
+    # P_bar[8, 8] = (10**(-8))**2
 
     # add uncertainty to the IC
     position_error = 5000 * np.divide(IC[0:3], norm(IC[0:3]))
@@ -352,11 +352,15 @@ def main():
         # the arguments for the filter are the IC, the first STM, the time span, the observables
         # data dictionary, a priori uncertainty, and the measurables' uncertainty,
         # as well as any extras
+        if itr==0:
+            extras['oldPost'] = np.zeros([len(t_span), 2])
+
         filter_inputs = (IC, phi0, t_span, obs_filter,\
                          P_bar, observation_uncertainty, x_bar, extras)
         # run filter function
         ref_state, est_state, extra_data = run_batch(filter_inputs)
 
+        extras['oldPost'] = extra_data['postfit residuals']
         # save all outputs into the dictionary with a name associated with the iteration
         filter_outputs[str(itr)] = {}
         filter_outputs[str(itr)]['ref_state'] = ref_state
@@ -387,6 +391,8 @@ def main():
 
         plot_data = extra_data
 
+        plot_data['postfit delta']= extra_data['postfit changes']
+        plot_data['states']      = est_state
         plot_data['truth']       = obs_data['truth']
         plot_data['beacon_list'] = obs_data['beacons']
         plot_data['t_span']      = t_span

DINObatch/P&L/vanilla_pl/init.py
@@ -228,7 +228,7 @@ def main():
     extras['line_direction'] = 1.
 
     # Are we using the real dynamics for the ref or the trueData
-    extras['realData']= 'OFF'
+    extras['realData']= 'ON'
 
     ##################################################################################
 
@@ -339,6 +339,9 @@ def main():
             IC = est_state[0, :]
             x_bar -= extra_data['x_hat_array'][0, :]
 
+        if itr==0:
+            extras['oldPost'] = np.zeros([len(t_span), 2])
+
         # the arguments for the filter are the IC, the first STM, the time span, the observables
         # data dictionary, a priori uncertainty, and the measurables' uncertainty,
         # as well as any extras
@@ -346,6 +349,7 @@ def main():
                          P_bar, observation_uncertainty, x_bar, extras)
         # run filter function
         ref_state, est_state, extra_data = run_batch(filter_inputs)
+        extras['oldPost'] = extra_data['postfit residuals']
 
         # save all outputs into the dictionary with a name associated with the iteration
         filter_outputs[str(itr)] = {}
@@ -377,6 +381,8 @@ def main():
 
         plot_data = extra_data
 
+        plot_data['postfit delta']= extra_data['postfit changes']
+        plot_data['states']      = est_state
         plot_data['truth']       = obs_data['truth']
         plot_data['beacon_list'] = obs_data['beacons']
         plot_data['t_span']      = t_span