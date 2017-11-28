2f190b2f500b33ddd4158d9a803da4483a01e586
atharris <anha5550@colorado.edu>
2017-11-28 13:16:00 -0700

    Revised AttitudeFilter class

---
+++ DINObatch/Attitude/AttitudeFilter.py
@@ -0,0 +1,108 @@
+import pytest
+import sys, os, inspect
+import matplotlib
+import numpy as np
+import ctypes
+import math
+import csv
+import logging
+
+# @cond DOXYGEN_IGNORE
+filename = inspect.getframeinfo(inspect.currentframe()).filename
+path = os.path.dirname(os.path.abspath(filename))
+
+bskName = 'Basilisk'
+bskPath = '../..' + '/' + bskName + '/'
+sys.path.append(bskPath + 'modules')
+sys.path.append(bskPath + 'PythonModules')
+
+import simulationArchTypes
+# if this script is run from a custom folder outside of the Basilisk folder, then uncomment the
+# following line and specify the absolute bath to the Basilisk folder
+#bskPath = '/Users/hp/Documents/Research/' + bskName + '/'
+# @endcond
+
+##  AttitudeFilter Class
+#   Inputs:
+#   IMU_data - IMUmessage - used for gyro reading.
+#   st_data - StarTracker message - used for star tracker reading.
+
+#   Outputs:
+#   nav_aggregate - provides estimated attitude, angular rates.
+
+class AttitudeFilter(simulationArchTypes.PythonModelClass):
+    def __init__(self, modelName, modelActive=True, modelPriority=-1):
+        super(PythonModule, self).__init__(modelName, modelActive, modelPriority)
+
+        ## Input gyro, star tracker message names
+        self.inputStName = ""
+        self.inputIMUName = ""
+
+        ## Output body torque message name
+        self.outputMsgName = ""
+
+        ## Input message ID (initialized to -1 to break messaging if unset)
+        self.inputStMsgID = -1
+        self.inputIMUMsgID = -1
+
+        ## Output message ID (initialized to -1 to break messaging if unset)
+        self.outputMsgID = -1
+
+        ## Input IMU, Star Tracker structures
+        self.inputIMUData = IMU.OutputMsgStruct()
+        self.inputStData = StarTracker.OutputMsgStruct()
+
+        ## Output navigation estimate structure.
+        self.outputMsgData = SimpleNav.OutputMsgStruct()
+
+        self.stateEst = np.zeros([6,]) #    state estimate is 3 delta MRPs, 3 bias states
+        self.outEst = np.zeros([6,]) #      Output is 3 BN MRPs, 3 angular rates
+        self.covarEst = np.zeros([6,6])
+
+        self.stateNoise = np.identity(6)
+        self.measNoise = np.identity(3)
+
+    ## The selfInit method is used to initialze all of the output messages of a class.
+    # It is important that ALL outputs are initialized here so that other models can
+    # subscribe to these messages in their crossInit method.
+    def selfInit(self):
+        self.outputMsgID = simulationArchTypes.CreateNewMessage(self.outputMsgName, self.outputMsgData,
+                                                                 self.moduleID)
+        return
+
+    ## The crossInit method is used to initialize all of the input messages of a class.
+    #  This subscription assumes that all of the other models present in a given simulation
+    #  instance have initialized their messages during the selfInit step.
+    def crossInit(self):
+        self.inputStMsgID = simulationArchTypes.SubscribeToMessage(self.inputStMsgName, self.inputStMsgData, self.moduleID)
+        self.inputIMUMsgID = simulationArchTypes.SubscribeToMessage(self.inputIMUMsgName, self.inputIMUMsgData, self.moduleID)
+        return
+
+    ## The reset method is used to clear out any persistent variables that need to get changed
+    #  when a task is restarted.  This method is typically only called once after selfInit/crossInit,
+    #  but it should be written to allow the user to call it multiple times if necessary.
+    def reset(self, currentTime):
+        return
+
+    ## The updateState method is the cyclical worker method for a given Basilisk class.  It
+    # will get called periodically at the rate specified in the Python task that the model is
+    # attached to.  It persists and anything can be done inside of it.  If you have realtime
+    # requirements though, be careful about how much processing you put into a Python updateState
+    # method.  You could easily detonate your sim's ability to run in realtime.
+    def updateState(self, currentTime):
+        #   First, read messages we've subscribed to:
+        simulationArchTypes.ReadMessage(self.inputMsgID, self.inputMsgData, self.moduleID)
+        simulationArchTypes.ReadMessage(self.inputMsgID, self.inputMsgData, self.moduleID)
+
+        #   Next, implement your routines or functions to process the input data and store it:
+        localInputData = self.inputMsgData
+        localOutputData = foo(localInputData)
+        self.outputMsgData = localOutputData
+
+
+        #   Finally, write the output message types:
+        simulationArchTypes.WriteMessage(self.outputMsgID, currentTime, self.outputMsgData, self.moduleID)
+
+    def kalmanStep(self):
+
+
