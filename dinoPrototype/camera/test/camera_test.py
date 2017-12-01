d46d05035480715fd15d0344266da394b106dc76
Matt <mrmuszynski@gmail.com>
2017-11-30 17:41:00 -0700

    started test 4.8

--- dinoPrototype/camera/test/camera_test.py
+++ dinoPrototype/camera/test/camera_test.py
@@ -34,6 +34,7 @@ import camera
 import numpy as np
 import matplotlib.pyplot as plt
 import pdb
+from adcs import Euler321_2DCM
 #create a spacecraft object to be shared across tests
 #a lot of the details of the spacecraft don't matter because
 #they aren't used in the camera model. The really important one is 
@@ -80,57 +81,57 @@ msg = { 'bodies': [
 	'addStars': 0,'rmOcc': 0, 'addBod': 0, 'psf': 1, 
 	'raster': 1, 'photon': 0, 'dark': 0, 'read': 0, 'dt': 0.01}
 
-#create camera with no stars in it for tests that don't need them
-#They will run significantly faster without them.
-noStarCam = camera.camera(
-	2, 					#detectorHeight
-	2, 					#detectorWidth
-	5.0, 				#focalLength
-	512, 				#resolutionHeight
-	512,				#resolutionWidth
-	np.identity(3), 	#body2cameraDCM
-	1000,		   	 	#maximum magnitude
-	-1000,				#minimum magnitude (for debugging)
-	qe,					#quantum efficiency dictionary
-	tc,					#transmission curve dictionary
-	1,					#wavelength bin size in nm
-	0.01**2, 			#effective area in m^2
-	100, 				#dark current in electrons per second
-	100, 				#std for read noise in electrons
-	100, 				#bin size
-	2**32, 				#max bin depth
-	1,					#sigma for gaussian psf
-	sc,					#spacecraft the camera is attached to
-	msg,				#debug message
-	db='../db/tycho.db'	#stellar database
-	)
+# #create camera with no stars in it for tests that don't need them
+# #They will run significantly faster without them.
+# noStarCam = camera.camera(
+# 	2, 					#detectorHeight
+# 	2, 					#detectorWidth
+# 	5.0, 				#focalLength
+# 	512, 				#resolutionHeight
+# 	512,				#resolutionWidth
+# 	np.identity(3), 	#body2cameraDCM
+# 	1000,		   	 	#maximum magnitude
+# 	-1000,				#minimum magnitude (for debugging)
+# 	qe,					#quantum efficiency dictionary
+# 	tc,					#transmission curve dictionary
+# 	1,					#wavelength bin size in nm
+# 	0.01**2, 			#effective area in m^2
+# 	100, 				#dark current in electrons per second
+# 	100, 				#std for read noise in electrons
+# 	100, 				#bin size
+# 	2**32, 				#max bin depth
+# 	1,					#sigma for gaussian psf
+# 	sc,					#spacecraft the camera is attached to
+# 	msg,				#debug message
+# 	db='../db/tycho.db'	#stellar database
+# 	)
 
-#now create a camera with stars in it for use in the tests that
-#actually need them.
-msg['addStars'] = 1
-starCam = camera.camera(
-	2, 					#detectorHeight
-	2, 					#detectorWidth
-	5.0, 				#focalLength
-	512, 				#resolutionHeight
-	512,				#resolutionWidth
-	np.identity(3), 	#body2cameraDCM
-	1000,		   	 	#maximum magnitude
-	-1000,				#minimum magnitude (for debugging)
-	qe,					#quantum efficiency dictionary
-	tc,					#transmission curve dictionary
-	1,					#wavelength bin size in nm
-	0.01**2, 			#effective area in m^2
-	100, 				#dark current in electrons per second
-	100, 				#std for read noise in electrons
-	100, 				#bin size
-	2**32, 				#max bin depth
-	1,					#sigma for gaussian psf
-	sc,					#spacecraft the camera is attached to
-	msg,				#debug message
-	db='../db/tycho.db'	#stellar database
-	)
-sc.attitudeDCM = np.identity(3)
+# #now create a camera with stars in it for use in the tests that
+# #actually need them.
+# msg['addStars'] = 1
+# starCam = camera.camera(
+# 	2, 					#detectorHeight
+# 	2, 					#detectorWidth
+# 	5.0, 				#focalLength
+# 	512, 				#resolutionHeight
+# 	512,				#resolutionWidth
+# 	np.identity(3), 	#body2cameraDCM
+# 	1000,		   	 	#maximum magnitude
+# 	-1000,				#minimum magnitude (for debugging)
+# 	qe,					#quantum efficiency dictionary
+# 	tc,					#transmission curve dictionary
+# 	1,					#wavelength bin size in nm
+# 	0.01**2, 			#effective area in m^2
+# 	100, 				#dark current in electrons per second
+# 	100, 				#std for read noise in electrons
+# 	100, 				#bin size
+# 	2**32, 				#max bin depth
+# 	1,					#sigma for gaussian psf
+# 	sc,					#spacecraft the camera is attached to
+# 	msg,				#debug message
+# 	db='../db/tycho.db'	#stellar database
+# 	)
+# sc.attitudeDCM = np.identity(3)
 
 # def test_4_1_loadAllStars():
 # 	#load support dict that was calculated offline
@@ -147,25 +148,25 @@ sc.attitudeDCM = np.identity(3)
 # 	assert(sum(starCam.VT) == test_4_1_support_dict['VTsum'])
 # 	assert(sum(starCam.BVT) == test_4_1_support_dict['BVTsum'])
 
-# def test_4_2_calculate_FOV():
-# 	cam1 = camera.camera(
-# 		1,2,2)
-# 	cam2= camera.camera(
-# 		3,4,5)
-# 	cam3 = camera.camera(
-# 		6,4,2)
+# # def test_4_2_calculate_FOV():
+# # 	cam1 = camera.camera(
+# # 		1,2,2)
+# # 	cam2= camera.camera(
+# # 		3,4,5)
+# # 	cam3 = camera.camera(
+# # 		6,4,2)
 
-# 	assert(cam1.angularHeight == 15)
-# 	assert(cam1.angularWidth == 16)
-# 	assert(cam1.angularDiagonal == 18)
+# # 	assert(cam1.angularHeight == 15)
+# # 	assert(cam1.angularWidth == 16)
+# # 	assert(cam1.angularDiagonal == 18)
 
-# 	assert(cam2.angularHeight == 54)
-# 	assert(cam2.angularWidth == 45)
-# 	assert(cam2.angularDiagonal == 25)
+# # 	assert(cam2.angularHeight == 54)
+# # 	assert(cam2.angularWidth == 45)
+# # 	assert(cam2.angularDiagonal == 25)
 
-# 	assert(cam2.angularHeight == 13)
-# 	assert(cam2.angularWidth == 17)
-# 	assert(cam2.angularDiagonal == 18)
+# # 	assert(cam2.angularHeight == 13)
+# # 	assert(cam2.angularWidth == 17)
+# # 	assert(cam2.angularDiagonal == 18)
 
 # def test_4_7_cameraUpdateState():
 # 	assert(len(noStarCam.images) == 0)
@@ -190,6 +191,114 @@ sc.attitudeDCM = np.identity(3)
 # 	assert(len(noStarCam.images) == 2)
 # 	assert(len(noStarCam.images[0].scenes) == 3)
 # 	assert(len(noStarCam.images[1].scenes) == 2)
+
+def test_4_8_findStarsInFOV():
+	msg = { 'bodies': [
+		bod.earth,
+		bod.luna,
+		sc
+		], 
+		'addStars': 1,'rmOcc': 0, 'addBod': 0, 'psf': 1, 
+		'raster': 1, 'photon': 0, 'dark': 0, 'read': 0, 'dt': 0.01}
+
+	OriCam = camera.camera(
+		1.5,				#detectorHeight
+		1.5, 				#detectorWidth
+		3.0, 				#focalLength
+		512, 				#resolutionHeight
+		512,				#resolutionWidth
+		np.identity(3), 	#body2cameraDCM
+		3.57,		   	 	#maximum magnitude
+		-1000,				#minimum magnitude (for debugging)
+		qe,					#quantum efficiency dictionary
+		tc,					#transmission curve dictionary
+		1,					#wavelength bin size in nm
+		0.01**2, 			#effective area in m^2
+		100, 				#dark current in electrons per second
+		100, 				#std for read noise in electrons
+		100, 				#bin size
+		2**32, 				#max bin depth
+		1,					#sigma for gaussian psf
+		sc,					#spacecraft the camera is attached to
+		msg,				#debug message
+		db='../db/tycho.db'	#stellar database
+		)
+	sc.attitudeDCM = Euler321_2DCM(
+		np.deg2rad(85),
+		np.deg2rad(0),
+		np.deg2rad(0)
+		)
+	
+	msg['takeImage'] = 1
+	OriCam.updateState()
+	msg['takeImage'] = 0
+	OriCam.updateState()
+
+
+	UMiCam = camera.camera(
+		2.3,				#detectorHeight
+		2.3,				#detectorWidth
+		4.0, 				#focalLength
+		512, 				#resolutionHeight
+		512,				#resolutionWidth
+		np.identity(3), 	#body2cameraDCM
+		4,			   	 	#maximum magnitude
+		-1000,				#minimum magnitude (for debugging)
+		qe,					#quantum efficiency dictionary
+		tc,					#transmission curve dictionary
+		1,					#wavelength bin size in nm
+		0.01**2, 			#effective area in m^2
+		100, 				#dark current in electrons per second
+		100, 				#std for read noise in electrons
+		100, 				#bin size
+		2**32, 				#max bin depth
+		1,					#sigma for gaussian psf
+		sc,					#spacecraft the camera is attached to
+		msg,				#debug message
+		db='../db/tycho.db'	#stellar database
+		)
+	sc.attitudeDCM = Euler321_2DCM(
+		np.deg2rad(187),
+		np.deg2rad(59),
+		np.deg2rad(0)
+		)
+
+	msg['takeImage'] = 1
+	UMiCam.updateState()
+	msg['takeImage'] = 0
+	UMiCam.updateState()
+
+
+	plot = 1
+
+	if plot:
+		plt.figure()
+		plt.imshow(OriCam.images[0].detectorArray.reshape(
+			OriCam.resolutionHeight,
+			OriCam.resolutionWidth
+			))
+		plt.figure()
+		plt.imshow(UMiCam.images[0].detectorArray.reshape(
+			UMiCam.resolutionHeight,
+			UMiCam.resolutionWidth
+			))
+
+		plt.figure()
+		plt.plot(OriCam.images[0].scenes[0].pixel,OriCam.images[0].scenes[0].line,'.')
+		plt.xlim(0,OriCam.resolutionWidth)
+		plt.ylim(0,OriCam.resolutionHeight)
+		plt.gca().invert_yaxis()
+		plt.axis('equal')
+		plt.figure()
+		plt.plot(UMiCam.images[0].scenes[0].pixel,UMiCam.images[0].scenes[0].line,'.')
+		plt.xlim(0,UMiCam.resolutionWidth)
+		plt.ylim(0,UMiCam.resolutionHeight)
+		plt.gca().invert_yaxis()
+		plt.axis('equal')
+	pdb.set_trace()
+
+	assert(1==1)
+
 
 
 # def test_4_9_imageRemoveOccultations():
@@ -382,82 +491,82 @@ sc.attitudeDCM = np.identity(3)
 # 	TSI = sum(pi*r_sun**2/au**2*bbCurve)
 # 	assert( abs((TSI - 1367)/1367) <0.001 )
 
-def test_4_20_checkFOV():
-	#remove moon from bodies message
-	msg['bodies'] = [bod.earth, sc]
-	msg['addBod'] = 1
+# def test_4_20_checkFOV():
+# 	#remove moon from bodies message
+# 	msg['bodies'] = [bod.earth, sc]
+# 	msg['addBod'] = 1
 
-	#position earth and sc so earth is at the center of the FOV
-	bod.earth.state = np.array([au/1000,0,0,0,0,0])
-	sc.state = bod.earth.state - np.array([100000,0,0,0,0,0])
-	msg['takeImage'] = 1
-	noStarCam.updateState()
-	msg['takeImage'] = 0
-	noStarCam.updateState()
+# 	#position earth and sc so earth is at the center of the FOV
+# 	bod.earth.state = np.array([au/1000,0,0,0,0,0])
+# 	sc.state = bod.earth.state - np.array([100000,0,0,0,0,0])
+# 	msg['takeImage'] = 1
+# 	noStarCam.updateState()
+# 	msg['takeImage'] = 0
+# 	noStarCam.updateState()
 
-	#position earth and sc so earth is completelt out of the FOV
-	bod.earth.state = np.array([au/1000,0,0,0,0,0])
-	sc.state = bod.earth.state - np.array([0,100000,0,0,0,0])
-	msg['takeImage'] = 1
-	noStarCam.updateState()
-	msg['takeImage'] = 0
-	noStarCam.updateState()
+# 	#position earth and sc so earth is completelt out of the FOV
+# 	bod.earth.state = np.array([au/1000,0,0,0,0,0])
+# 	sc.state = bod.earth.state - np.array([0,100000,0,0,0,0])
+# 	msg['takeImage'] = 1
+# 	noStarCam.updateState()
+# 	msg['takeImage'] = 0
+# 	noStarCam.updateState()
 
-	#position earth and sc so earth is completelt out of the FOV
-	bod.earth.state = np.array([au/1000,0,0,0,0,0])
-	sc.state = bod.earth.state - np.array([100000,23000,0,0,0,0])
-	msg['takeImage'] = 1
-	noStarCam.updateState()
-	msg['takeImage'] = 0
-	noStarCam.updateState()
+# 	#position earth and sc so earth is completelt out of the FOV
+# 	bod.earth.state = np.array([au/1000,0,0,0,0,0])
+# 	sc.state = bod.earth.state - np.array([100000,23000,0,0,0,0])
+# 	msg['takeImage'] = 1
+# 	noStarCam.updateState()
+# 	msg['takeImage'] = 0
+# 	noStarCam.updateState()
 
-	#position earth and sc so earth is completelt out of the FOV
-	bod.earth.state = np.array([au/1000,0,0,0,0,0])
-	sc.state = bod.earth.state - np.array([100000,0,23000,0,0,0])
-	msg['takeImage'] = 1
-	noStarCam.updateState()
-	msg['takeImage'] = 0
-	noStarCam.updateState()
+# 	#position earth and sc so earth is completelt out of the FOV
+# 	bod.earth.state = np.array([au/1000,0,0,0,0,0])
+# 	sc.state = bod.earth.state - np.array([100000,0,23000,0,0,0])
+# 	msg['takeImage'] = 1
+# 	noStarCam.updateState()
+# 	msg['takeImage'] = 0
+# 	noStarCam.updateState()
 
-	plot = 1
-	if plot:
-		plt.figure()
-		plt.imshow(noStarCam.images[0].detectorArray.reshape(
-			noStarCam.resolutionWidth,
-			noStarCam.resolutionHeight), cmap='Greys_r')
-		plt.xlabel('Pixel')
-		plt.ylabel('Line')
-		plt.title('Body Fully In View')
+# 	plot = 1
+# 	if plot:
+# 		plt.figure()
+# 		plt.imshow(noStarCam.images[2].detectorArray.reshape(
+# 			noStarCam.resolutionWidth,
+# 			noStarCam.resolutionHeight), cmap='Greys_r')
+# 		plt.xlabel('Pixel')
+# 		plt.ylabel('Line')
+# 		plt.title('Body Fully In View')
 
-		plt.figure()
-		plt.imshow(noStarCam.images[1].detectorArray.reshape(
-			noStarCam.resolutionWidth,
-			noStarCam.resolutionHeight), cmap='Greys_r')
-		plt.xlabel('Pixel')
-		plt.ylabel('Line')
-		plt.title('Body Fully Out of View')
+# 		plt.figure()
+# 		plt.imshow(noStarCam.images[3].detectorArray.reshape(
+# 			noStarCam.resolutionWidth,
+# 			noStarCam.resolutionHeight), cmap='Greys_r')
+# 		plt.xlabel('Pixel')
+# 		plt.ylabel('Line')
+# 		plt.title('Body Fully Out of View')
 
-		plt.figure()
-		plt.imshow(noStarCam.images[2].detectorArray.reshape(
-			noStarCam.resolutionWidth,
-			noStarCam.resolutionHeight), cmap='Greys_r')
-		plt.xlabel('Pixel')
-		plt.ylabel('Line')
-		plt.title('Body Center Out of View (Pixel Dimension)')
+# 		plt.figure()
+# 		plt.imshow(noStarCam.images[4].detectorArray.reshape(
+# 			noStarCam.resolutionWidth,
+# 			noStarCam.resolutionHeight), cmap='Greys_r')
+# 		plt.xlabel('Pixel')
+# 		plt.ylabel('Line')
+# 		plt.title('Body Center Out of View (Pixel Dimension)')
 
-		plt.figure()
-		plt.imshow(noStarCam.images[3].detectorArray.reshape(
-			noStarCam.resolutionWidth,
-			noStarCam.resolutionHeight), cmap='Greys_r')
-		plt.xlabel('Pixel')
-		plt.ylabel('Line')
-		plt.title('Body Center Out of View (Line Dimension)')
+# 		plt.figure()
+# 		plt.imshow(noStarCam.images[5].detectorArray.reshape(
+# 			noStarCam.resolutionWidth,
+# 			noStarCam.resolutionHeight), cmap='Greys_r')
+# 		plt.xlabel('Pixel')
+# 		plt.ylabel('Line')
+# 		plt.title('Body Center Out of View (Line Dimension)')
 
-	pdb.set_trace()
-	assert(len(noStarCam.images[0].scenes[0].pixel) != 0)
-	assert(len(noStarCam.images[1].scenes[0].pixel) == 0)
-	assert(len(noStarCam.images[2].scenes[0].pixel) != 0)
-	assert(len(noStarCam.images[3].scenes[0].pixel) != 0)
+# 	pdb.set_trace()
+# 	assert(len(noStarCam.images[2].scenes[0].pixel) != 0)
+# 	assert(len(noStarCam.images[3].scenes[0].pixel) == 0)
+# 	assert(len(noStarCam.images[4].scenes[0].pixel) != 0)
+# 	assert(len(noStarCam.images[5].scenes[0].pixel) != 0)
 
 # def test_4_21_mapSphere():
 # 	import lightSimFunctions
