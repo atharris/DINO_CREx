fixing DE angles now that camera.py is fixed. When putting DE into a 3-2-1 euler angle DCM, you have to flip the sign on DE since positive DEE is actually a negative rotation and vice versa.

dinoModels/SimCode/opnavCamera/test/camera_test.py
@@ -218,7 +218,6 @@ def test_4_7_cameraUpdateState():
 	assert(len(noStarCam.images[0].scenes) == 3)
 	assert(len(noStarCam.images[1].scenes) == 2)
 
-pdb.set_trace()
 def test_4_8_findStarsInFOV():
 	msg = { 'bodies': [
 		bod.earth,
@@ -293,7 +292,7 @@ def test_4_8_findStarsInFOV():
 		)
 	UMiCam.scDCM = Euler321_2DCM(
 		np.deg2rad(187),
-		np.deg2rad(59),
+		np.deg2rad(-59),
 		np.deg2rad(0)
 		)
 
@@ -477,7 +476,7 @@ def test_4_9_imageRemoveOccultations():
 # set up image for tests 10-12
 tinyCam.scDCM = Euler321_2DCM(
 	np.deg2rad(1.12551889),
-	np.deg2rad(2.26739556),
+	np.deg2rad(-2.26739556),
 	np.deg2rad(0)
 	)
 tinyCam.takeImage = 1